e5928ff8c9a18a3e7bdccbbf3810b1001f865c02
Onur Bönceoğlu <OnurBonceoglu@hotmail.com>
2021-09-01 23:38:22 +0300

    Add project files.

---
+++ Kelime Karıştırıcı/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Kelime_Karıştırıcı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.sira = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kelime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(26, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mix";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.sira,
+            this.kelime});
+            this.dataGridView1.Location = new System.Drawing.Point(221, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(321, 173);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // sira
+            // 
+            this.sira.HeaderText = "Sıra";
+            this.sira.Name = "sira";
+            this.sira.ReadOnly = true;
+            this.sira.Width = 60;
+            // 
+            // kelime
+            // 
+            this.kelime.HeaderText = "Kelime";
+            this.kelime.Name = "kelime";
+            this.kelime.ReadOnly = true;
+            this.kelime.Width = 200;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(37, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Karıştırılacak Kelime";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(310, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Oluşturulan Kelimeler";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(22, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Durum:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(77, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "İşlem bekleniyor";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(221, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(321, 39);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Kelimeleri XML Olarak Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 308);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kelime Karıştırıcı - OnurBönceoğlu";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sira;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kelime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Kelime Karıştırıcı/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Kelime_Karıştırıcı
+{
+    public partial class Form1 : Form
+    {
+        List<string> finalWordList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mainWord = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(mainWord))
+            {
+                return;
+            }
+            label4.Text = "Kelimeler oluşturuluyor, lütfen bekleyiniz...";
+
+            CreateTheWords(mainWord.Trim());
+
+            PlaySound();
+        }
+
+        void CreateTheWords(string mainWord)
+        {
+            int mainWordLength = mainWord.Length;
+            List<string> mainLetterList = new List<string>();
+            finalWordList = new List<string>();
+
+            for (int i = 0; i < mainWord.Length; i++)
+            {
+                mainLetterList.Add(mainWord.Substring(i, 1));
+            }
+
+            int factorial = 1;
+            for (int i = 1; i <= mainWordLength; i++)
+            {
+                factorial *= i;
+            }
+
+
+
+            List<string> letters = new List<string>(mainLetterList);
+
+            Random rnd = new Random();
+            dataGridView1.Rows.Clear();
+            int totalLoopCount = factorial * 10; //100 
+
+            if (totalLoopCount > 10000)
+            {
+                totalLoopCount = 10000;
+            }
+            for (int i = 0; i < totalLoopCount; i++)
+            {
+                letters = new List<string>(mainLetterList);
+
+                string wordToAdd = "";
+
+                while (letters.Count > 0)
+                {
+                    int index = rnd.Next(0, letters.Count);
+
+                    wordToAdd = wordToAdd + letters[index];
+
+                    letters.RemoveAt(index);
+                }
+
+                if (!finalWordList.Contains(wordToAdd))
+                {
+                    finalWordList.Add(wordToAdd);
+                    //listBox1.Items.Add(wordToAdd); //
+                    dataGridView1.Rows.Add(finalWordList.Count, wordToAdd);
+                }
+            }
+
+            //listBox1.Refresh();
+            dataGridView1.Refresh();
+            label4.Text = $"{finalWordList.Count} kelime oluşturuldu";
+        }
+
+        void SaveWordsToXML()
+        {
+            List<string> data = new List<string>(finalWordList); // ... // populate the list
+
+            //create the serialiser to create the xml
+            XmlSerializer serialiser = new XmlSerializer(typeof(List<string>));
+
+            // Create the TextWriter for the serialiser to use
+            TextWriter filestream = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "Kelimeler.xml");
+
+            //write to the file
+            serialiser.Serialize(filestream, data);
+
+            label4.Text = $"Dosya {AppDomain.CurrentDomain.BaseDirectory}Kelimeler.xml konumuna kaydedildi.";
+
+            PlaySound();
+
+            // Close the file
+            filestream.Close();
+        }
+
+        void PlaySound()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(AppDomain.CurrentDomain.BaseDirectory + "Sound.wav");
+            player.Play();
+        }
+
+        public string ToXML<T>(T obj)
+        {
+            using (StringWriter stringWriter = new StringWriter(new StringBuilder()))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                xmlSerializer.Serialize(stringWriter, obj);
+                return stringWriter.ToString();
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveWordsToXML();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
